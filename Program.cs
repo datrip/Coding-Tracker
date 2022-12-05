0fda21e094dcfcb207c3bffd588138a5a76475fc
Tomas Paliunas <tp19694@essex.ac.uk>
2022-12-05 19:48:53 +0200

    added app.config file and database creation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Data.SQLite;
+using System.Configuration;
+using System.Collections.Specialized;
+using System;
+using System.Text;
 
 namespace Coding_Tracker // Note: actual namespace depends on the project name.
 {
@@ -6,7 +10,29 @@ namespace Coding_Tracker // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var conn = ConfigurationManager.ConnectionStrings["Coding-Tracking"].ConnectionString;
+            {
+
+
+                using (var connection = new SQLiteConnection(conn))
+                {
+
+                    connection.Open();
+                    var tableCmd = connection.CreateCommand();
+
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS coding_tracker (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        StartTime TEXT,
+                        EndTime TEXT,
+                        Duration TEXT
+                        )";
+
+                    tableCmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
         }
     }
 }
